dacaf4cd856317efbc96981e0ceec29d8183d802
Marat Minchik <devminrat@gmail.com>
2021-06-22 19:12:49 +0300

    added start UI elements and styles

---
+++ app.js
@@ -0,0 +1,17 @@
+// function getWeather(cb) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open(
+//     "GET",
+//     "http://api.openweathermap.org/data/2.5/weather?q=Пувиак&appid=9fcc82b3417048f0f7b58aa6dc1ad2b3&units=metric&lang=ru"
+//   );
+//   xhr.addEventListener("load", () => {
+//     const response = JSON.parse(xhr.responseText);
+//     cb(response);
+//   });
+
+//   xhr.send();
+// }
+
+// getWeather((response) => {
+//   console.log(response);
+// });
