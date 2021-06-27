910e82c597a3b4b3c15485d2b7753abe4fb7ce70
Marat Minchik <devminrat@gmail.com>
2021-06-27 22:22:43 +0300

    added some weather img and their stylization

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //   const xhr = new XMLHttpRequest();
 //   xhr.open(
 //     "GET",
-//     "http://api.openweathermap.org/data/2.5/weather?q=Пувиак&appid=9fcc82b3417048f0f7b58aa6dc1ad2b3&units=metric&lang=ru"
+//     "http://api.openweathermap.org/data/2.5/weather?q=Минск&appid=9fcc82b3417048f0f7b58aa6dc1ad2b3&units=metric&lang=ru"
 //   );
 //   xhr.addEventListener("load", () => {
 //     const response = JSON.parse(xhr.responseText);
